4fd9aac6799fa0882f503c516f6d2c5c1d9bcf90
EDWARD WEBB <EDWARD.WEBB@LibertyMutual.com>
2017-11-09 17:00:06 -0500

    adds subscribers, but multiple component subscription is an issue since pytho object only allows single unique key

--- migrate.py
+++ migrate.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python
 import requests
+import time
 
-source_pages = ["gplkhyvn1nh2"]
+source_pages = ["gplkhyvn1nh2","vfdq0lwd1hk7","46qjhlfcz2w3","mxyt2m7f2kvz"]
+#source_pages = ["mxyt2m7f2kvz"]
 target_page = "hk277w25hzw7"
 api_key = "20a337bf86ccf339f4c4c25be8d4a47fd38aa2949c682e601260c1a1de23f237"
-group_id_mappings = {}
-
+parent_ids = []
+id_mappings = {}
+subscriber_by_key = {}
 
 from requests.auth import AuthBase
 
@@ -18,6 +21,7 @@ class ApiKeyAuth(AuthBase):
     def __call__(self, r):
         # modify and return the request
         r.headers['Authorization'] = "OAuth " + self.api_key
+        time.sleep(1.1) #hack ensures we dont exceed rate limit for our token
         return r
 
 
@@ -44,8 +48,15 @@ def getComponents(page_id):
 
 def postComponent(payload):
 	url = "https://api.statuspage.io/v1/pages/" + target_page + "/components.json"
-	print "sending"
-	print payload
+	return post(url,payload)
+
+
+def getSubscriptions(page_id):
+	url = "https://api.statuspage.io/v1/pages/" + page_id + "/subscribers.json"
+	return get(url)
+
+def postSubscriber(payload):
+	url = "https://api.statuspage.io/v1/pages/" + target_page + "/subscribers.json"
 	return post(url,payload)
 
 def createComponent(component,allComponents):
@@ -55,39 +66,84 @@ def createComponent(component,allComponents):
 		'component[description]':component['description']
 		}
 	if component['group_id']:
-		if group_id_mappings[component['group_id']]:
-			print "\tAdding previously created parent ID " + group_id_mappings[component['group_id']]
-			payload['component[group_id]'] = group_id_mappings[component['group_id']]
+		if component['group_id'] in id_mappings:
+			print "\tRelating to previously created parent ID " + id_mappings[component['group_id']]
+			payload['component[group_id]'] = id_mappings[component['group_id']]
 		else:
 			print "\tAdding new parent name: " + allComponents[component['group_id']]['name']
 			payload['component[group_name]'] = allComponents[component['group_id']]['name']
+			#payload['component[group_description]'] = allComponents[component['group_id']]['description']
 	else:
 		# becuase it has no parent, it could be a parent
-		if component['id'] in group_id_mappings:
+		if component['id'] in parent_ids:
 			print "\tSkipping parent: " + component['name']
 			return
 	response = postComponent(payload)
+	print "\t" + str(response.status_code)
+	if response.status_code != 201:
+		print response.text
 	new_component = response.json()
+	id_mappings[component['id']] = new_component['id']
 	if 'group_id' in new_component:
-		group_id_mappings[component['group_id']] = new_component['group_id']
+		id_mappings[component['group_id']] = new_component['group_id']
+
+def createSubscriber(subscriber_key):
+	subscriber = subscriber_by_key[subscriber_key]
+	#note this leave user inactive, and you'll need to manually or use /subscribers/reactivate to enable them
+	payload={'subscriber[skip_confirmation_notification]':'true'}
+	if 'phone_number' in subscriber:
+		payload['subscriber[phone_number]'] = subscriber['phone_number']
+		payload['subscriber[phone_country]'] = subscriber['phone_country']
+	else:
+		payload['subscriber[email]'] = subscriber['email']
+	for old_component_id in subscriber['components']:
+		payload['subscriber[component_ids][]'] = id_mappings[old_component_id]
+	response = postSubscriber(payload)
+	print "\t" + str(response.status_code)
+	if response.status_code != 201:
+		print response.text
+
+
+def addOrMergeSubscriber(subscriber,allPageComponentsById):
+	print "\tcapturing subscriber details: " + subscriber['id']
+	key=""
+	if 'phone_number' in subscriber:
+		key=subscriber['phone_number']
+	else:
+		key=subscriber['email']
+	if 'components' not in subscriber:
+		print "\t WARN: grandfathered subscriber with empty components, will subscribe to all components for this page."
+		subscriber['components'] = allPageComponentsById.keys()
+	if key in subscriber_by_key:
+		target_subscriber = subscriber_by_key[key]
+		subscriber['components'] = subscriber['components']  + target_subscriber['components']
+	subscriber_by_key[key] = subscriber
+
 
 
 
 
 for page in source_pages:
-	print "Migrating page " + page
+	print "\nMigrating page " + page
+
 	response = getComponents(page)
 	allComponentsById = dict_components_by_id(response.json())
-
-
-
 	for component in response.json():
 		if component['group_id']:
-			if component['group_id'] not in group_id_mappings:
+			if component['group_id'] not in parent_ids:
 				print "marking component ID: " + component['group_id'] + " as a parent"
-				group_id_mappings[component['group_id']]=""
-
+				parent_ids.append(component['group_id'])
 	for component in response.json():
 		createComponent(component,allComponentsById)
+
+	response = getSubscriptions(page)
+	for subscriber in response.json():
+		addOrMergeSubscriber(subscriber,allComponentsById)
+
+
+for subscriber_key in subscriber_by_key:
+	print "Adding subscriber: " + subscriber_by_key[subscriber_key]['id'];
+	createSubscriber(subscriber_key)
+
 
 
